1e2982ced6143b29e536c2f601f67d6fe9da8f4d
ChristianRivero <christianriveroarnez@gmail.com>
2022-08-19 19:35:22 -0400

    added clear button

--- src/presenter.js
+++ src/presenter.js
@@ -8,10 +8,19 @@ const Price = document.querySelector("#ProductsPrice");
 const StateCodeCombo = document.querySelector("#StateCode");
 const form = document.querySelector("#TotalizatorForm");
 const ansDiv=document.querySelector("#AnswerDiv")
-const AlertDiv=document.querySelector("#AlertDiv")
+const ClearButton=document.querySelector("#ClearButton")
 document.onload=loadStates()
+
+ClearButton.addEventListener("click",()=>
+{
+    ansDiv.innerHTML=""
+    Qty.value=""
+    Price.value=""
+})
 Qty.addEventListener("input",()=>validateIfValuesIsNumber(Qty.value))
 Price.addEventListener("input",()=>validateIfValuesIsNumber(Price.value))
+
+
 form.addEventListener("submit",(event)=>
 {
     event.preventDefault()
