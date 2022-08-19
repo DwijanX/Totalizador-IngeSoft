751609bb3b8ce2194388945d068983ebf19c7f09
ChristianRivero <christianriveroarnez@gmail.com>
2022-08-19 18:04:52 -0400

    Added price field

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,10 @@
 
 const Qty = document.querySelector("#QtyProduct");
+const Price = document.querySelector("#ProductsPrice");
 const form = document.querySelector("#TotalizatorForm");
 const ansDiv=document.querySelector("#AnswerDiv")
 form.addEventListener("submit",(event)=>
 {
     event.preventDefault()
-    ansDiv.innerHTML="<p>"+Qty.value+"</p>"
+    ansDiv.innerHTML="<p>"+"Cantidad: "+Qty.value+"<br>Precio:"+Price.value+"</p>"
 });
