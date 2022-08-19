57412e473f2ad70379e3997d37bca045b8bcc752
ChristianRivero <christianriveroarnez@gmail.com>
2022-08-19 18:29:29 -0400

    More states added and a field to input the state

--- src/presenter.js
+++ src/presenter.js
@@ -2,6 +2,7 @@ import { getSubTotalPrice,getTotalPrice} from "./TotalPriceCalculator";
 import getTax from "./taxCalculator";
 const Qty = document.querySelector("#QtyProduct");
 const Price = document.querySelector("#ProductsPrice");
+const StateCode = document.querySelector("#StateCode");
 const form = document.querySelector("#TotalizatorForm");
 const ansDiv=document.querySelector("#AnswerDiv")
 form.addEventListener("submit",(event)=>
@@ -9,14 +10,14 @@ form.addEventListener("submit",(event)=>
     let QtyValue=Qty.value
     let PriceValue=Price.value
     let SubTotalPrice=getSubTotalPrice(QtyValue,PriceValue)
-    let TaxAmount=getTax(SubTotalPrice,"TX")
+    let TaxAmount=getTax(SubTotalPrice,StateCode.value)
     let TotalPrice=getTotalPrice(SubTotalPrice,TaxAmount)
     event.preventDefault()
     ansDiv.innerHTML=("<p>"+
     "Cantidad: "+QtyValue+
     "<br>Precio: "+PriceValue+
     "<br>Monto: "+SubTotalPrice+
-    "<br>Tax TX: "+TaxAmount+
+    "<br>Tax "+StateCode.value+": "+TaxAmount+
     "<br>Monto Total "+TotalPrice+
     "</p>")
 });
--- src/taxCalculator.js
+++ src/taxCalculator.js
@@ -1,4 +1,4 @@
-const USATaxes={TX:0.0625}
+const USATaxes={TX:0.0625,UT:0.0665,NV:0.08,AL:0.04,CA:0.0825}
 
 function getTax(Price,State)
 {
